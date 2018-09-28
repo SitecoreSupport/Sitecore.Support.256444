758ed5813a841205e996ad5ecc02374ac2fa04b4
DK\IEKA <ieka@sitecore.net>
2018-09-28 18:33:53 +0300

    Fix "NullReferenceException"  for "GetSiteLevelCaсhing" processor.

---
+++ src/Sitecore.Support.256444/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.256444")]
+[assembly: AssemblyProduct("Sitecore.Support.256444")]
+[assembly: ComVisible(false)]
